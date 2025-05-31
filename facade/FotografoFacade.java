c2265ef2adf4023391852afa89c6152ffb3e8003
JoaoGLopes12 <162383232+JoaoGLopes12@users.noreply.github.com>
2025-05-30 22:46:35 -0300

    Update FotografoFacade.java

--- facade/FotografoFacade.java
+++ facade/FotografoFacade.java
@@ -1,34 +1,45 @@
 package br.upf.projetofotografia.facade;
 
-import br.upf.projetofotografia.entity.FotoEntity;
+import br.upf.projetofotografia.entity.FotografoEntity;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import java.util.List;
 
 @Stateless
-public class FotoFacade {
+public class FotografoFacade {
 
     @PersistenceContext(unitName = "ProjetofotografiaPU")
     private EntityManager em;
 
-    public void create(FotoEntity entity) {
+    public void create(FotografoEntity entity) {
         em.persist(entity);
     }
 
-    public void edit(FotoEntity entity) {
+    public void edit(FotografoEntity entity) {
         em.merge(entity);
     }
 
-    public void remove(FotoEntity entity) {
+    public void remove(FotografoEntity entity) {
         em.remove(em.merge(entity));
     }
 
-    public FotoEntity find(Object id) {
-        return em.find(FotoEntity.class, id);
+    public FotografoEntity find(Object id) {
+        return em.find(FotografoEntity.class, id);
     }
 
-    public List<FotoEntity> findAll() {
-        return em.createQuery("SELECT f FROM FotoEntity f", FotoEntity.class).getResultList();
+    public List<FotografoEntity> findAll() {
+        return em.createQuery("SELECT f FROM FotografoEntity f", FotografoEntity.class).getResultList();
+    }
+
+    public FotografoEntity findByEmailAndSenha(String email, String senha) {
+        try {
+            return em.createQuery("SELECT f FROM FotografoEntity f WHERE f.email = :email AND f.senha = :senha", FotografoEntity.class)
+                .setParameter("email", email)
+                .setParameter("senha", senha)
+                .getSingleResult();
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
