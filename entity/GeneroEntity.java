30a5eff2e22c4c1aa5d157a8ffad81cfb202666a
JoaoGLopes12 <162383232+JoaoGLopes12@users.noreply.github.com>
2025-06-02 19:55:56 -0300

    Update GeneroEntity.java

--- entity/GeneroEntity.java
+++ entity/GeneroEntity.java
@@ -1,64 +1,22 @@
-package br.upf.projetojfprimefaces.entity;
+package br.upf.projetofotografia.entity;
 
+import jakarta.persistence.*;
 import java.io.Serializable;
-import java.util.List;
-import javax.persistence.*;
 
 @Entity
 @Table(name = "genero")
 public class GeneroEntity implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-    @Column(nullable = false, length = 100)
     private String descricao;
 
-    @OneToMany(mappedBy = "genero")
-    private List<FotoEntity> fotos;
-
     // Getters e Setters
-    public Long getId() {
-        return id;
-    }
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public List<FotoEntity> getFotos() {
-        return fotos;
-    }
-
-    public void setFotos(List<FotoEntity> fotos) {
-        this.fotos = fotos;
-    }
-
-    @Override
-    public int hashCode() {
-        return (id != null ? id.hashCode() : 0);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof GeneroEntity)) return false;
-        GeneroEntity other = (GeneroEntity) obj;
-        return (this.id != null && this.id.equals(other.id));
-    }
-
-    @Override
-    public String toString() {
-        return "br.upf.projetojfprimefaces.entity.GeneroEntity[ id=" + id + " ]";
-    }
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
 }
