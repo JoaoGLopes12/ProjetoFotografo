9c0d682c041e0b2e0b2c946ac54f6197dba5b51c
JoaoGLopes12 <162383232+JoaoGLopes12@users.noreply.github.com>
2025-06-02 20:07:09 -0300

    Update LoginController.java
    Substituído LoginController anterior baseado em PessoaEntity por nova versão com FotografoEntity

--- controller/LoginController.java
+++ controller/LoginController.java
@@ -1,71 +1,46 @@
-//tetse comit
-package br.upf.projetojfprimefaces.controller;
+package br.upf.projetofotografia.controller;
 
-import br.upf.projetojfprimefaces.entity.PessoaEntity;
-import br.upf.projetojfprimefaces.facade.PessoaFacade;
-import jakarta.enterprise.context.SessionScoped;
-import jakarta.inject.Inject;
-import jakarta.inject.Named;
-import java.io.Serializable;
+import br.upf.projetofotografia.entity.FotografoEntity;
+import br.upf.projetofotografia.facade.FotografoFacade;
+import jakarta.ejb.EJB;
+import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
-import jakarta.servlet.http.HttpSession;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.SessionScoped;
+import java.io.IOException;
+import java.io.Serializable;
 
 @Named
 @SessionScoped
 public class LoginController implements Serializable {
 
-    private String email;
-    private String senha;
+    @EJB
+    private FotografoFacade fotografoFacade;
 
-    private PessoaEntity pessoaLogada;
+    private FotografoEntity fotografo = new FotografoEntity();
 
-    @Inject
-    private PessoaFacade pessoaFacade;
+    public FotografoEntity getFotografo() {
+        return fotografo;
+    }
 
-    public String login() {
-        PessoaEntity pessoa = pessoaFacade.findByEmail(email);
-        if (pessoa != null && pessoa.getSenha().equals(senha)) {
-            pessoaLogada = pessoa;
-            HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
-            session.setAttribute("pessoaLogada", pessoaLogada);
-            return "areaAdministrativa.xhtml?faces-redirect=true";
+    public void setFotografo(FotografoEntity fotografo) {
+        this.fotografo = fotografo;
+    }
+
+    public String validarLogin() {
+        FotografoEntity usuario = fotografoFacade.findByEmailAndSenha(fotografo.getEmail(), fotografo.getSenha());
+        if (usuario != null) {
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("fotografoLogado", usuario);
+            return "/admin/foto.xhtml?faces-redirect=true";
         } else {
-            // Aqui você pode adicionar mensagem de erro para o usuário
-            FacesContext.getCurrentInstance().addMessage(null, 
-                new jakarta.faces.application.FacesMessage("Email ou senha inválidos."));
-            return "login.xhtml";
+            FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login inválido", "Email ou senha incorretos"));
+            return null;
         }
     }
 
     public String logout() {
-        pessoaLogada = null;
         FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-        return "login.xhtml?faces-redirect=true";
-    }
-
-    // Getters e Setters
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public PessoaEntity getPessoaLogada() {
-        return pessoaLogada;
-    }
-
-    public void setPessoaLogada(PessoaEntity pessoaLogada) {
-        this.pessoaLogada = pessoaLogada;
+        return "/login.xhtml?faces-redirect=true";
     }
 }
