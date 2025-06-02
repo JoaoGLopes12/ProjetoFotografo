5a40c50181aec355cf3b7fbd1f66de2fa502e733
JoaoGLopes12 <162383232+JoaoGLopes12@users.noreply.github.com>
2025-06-02 19:59:56 -0300

    Update LocalEntity.java

--- entity/LocalEntity.java
+++ entity/LocalEntity.java
@@ -1,64 +1,21 @@
-package br.upf.projetojfprimefaces.entity;
+package br.upf.projetofotografia.entity;
 
+import jakarta.persistence.*;
 import java.io.Serializable;
-import java.util.List;
-import javax.persistence.*;
 
 @Entity
 @Table(name = "local")
 public class LocalEntity implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-    @Column(nullable = false, length = 150)
     private String nome;
 
-    @OneToMany(mappedBy = "local")
-    private List<FotoEntity> fotos;
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
 
-    // Getters e Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public List<FotoEntity> getFotos() {
-        return fotos;
-    }
-
-    public void setFotos(List<FotoEntity> fotos) {
-        this.fotos = fotos;
-    }
-
-    @Override
-    public int hashCode() {
-        return (id != null ? id.hashCode() : 0);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof LocalEntity)) return false;
-        LocalEntity other = (LocalEntity) obj;
-        return (this.id != null && this.id.equals(other.id));
-    }
-
-    @Override
-    public String toString() {
-        return "br.upf.projetojfprimefaces.entity.LocalEntity[ id=" + id + " ]";
-    }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
 }
